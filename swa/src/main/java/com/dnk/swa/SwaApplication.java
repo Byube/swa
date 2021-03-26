fcd58f2fbee817a4a9d9538dfc0b8b220d4846d3
byube <kangjunne@naver.com>
2021-03-26 13:13:23 +0900

    war 테스트1

--- swa/src/main/java/com/dnk/swa/SwaApplication.java
+++ swa/src/main/java/com/dnk/swa/SwaApplication.java
@@ -1,24 +1,24 @@
 package com.dnk.swa;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class SwaApplication extends SpringBootServletInitializer{
 
-	
-	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		// TODO Auto-generated method stub
-		return builder.sources(SwaApplication.class);
+		return builder.sources(SpringApplicationBuilder.class);
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(SwaApplication.class, args);
-	}
+	
 	
 	
 
